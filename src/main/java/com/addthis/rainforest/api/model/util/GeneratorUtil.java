7594e51e909617f15c295ec8b6f3d14e99902e70
Brad Sneade <brad@addthis.com>
2015-09-30 10:11:39 -0400

    Updating Generators to provide some correct return types and friendlier method names

---
+++ src/main/java/com/addthis/rainforest/api/model/util/GeneratorUtil.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.addthis.rainforest.api.model.util;
+
+import com.addthis.rainforest.api.model.Column;
+import com.addthis.rainforest.api.model.Generator;
+
+/**
+ * Helper methods for dealing with {@link Generator}s.
+ */
+public class GeneratorUtil {
+    
+    /**
+     * Find the column id by name for a column in a generator.
+     * @param generator that holds the columns
+     * @param columnName to find
+     * @return the id if found, null if not
+     */
+    public static Long findColumnId(final Generator generator, final String columnName) {
+        for (final Column column : generator.getColumns()) {
+            if (columnName.equals(column.getName())) {
+                return column.getId();
+            }
+        }
+        return null;
+    }
+}
